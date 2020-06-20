67d64e11d485fe28d0a82ffc191e42e8a4c21727
techrepublik <joshlorilla@gmail.com>
2020-06-20 13:44:05 +0800

    to-heroku-07

--- client/forms.py
+++ client/forms.py
@@ -6,9 +6,14 @@ from .models import Client, Status
 class ClientForm(forms.ModelForm):
     class Meta:
         model       = Client
-        fields      = ['client_no', 'client_name', 'client_address', 'client_email', 'client_registered', 'client_ip', 'client_active']
+        fields      = ['client_no', 'client_name', 'client_address', 'client_email', 'client_registered', 'client_ip', 'client_contact', 'client_active']
         widgets = {
-            'client_registered' : DateInput(),
+            'client_no': forms.TextInput(attrs={'class':'form-control form-control-sm'}),
+            'client_name': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'client_email': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'client_registered' : DateInput(attrs={'class': 'form-control form-control-sm'}),
+            'client_ip': forms.TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'client_contact': forms.TextInput(attrs={'class':'form-control form-control-sm'}),
             'client_address': forms.Textarea(attrs={'rows':2})
         }
 
